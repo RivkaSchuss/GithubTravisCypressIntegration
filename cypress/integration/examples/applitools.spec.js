058730c58d5353e90f7cb4336d31f61ff8131567
RivkaSchuss <rschuss123@gmail.com>
2019-10-22 13:31:23 +0300

    CI integration

--- cypress/integration/examples/applitools.spec.js
+++ cypress/integration/examples/applitools.spec.js
@@ -2,7 +2,6 @@ describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
     cy.visit('https://applitools.com/helloworld');
     cy.eyesOpen({
-      apiKey: 'ipnvklZBLcUmg110VwqiLxhJAdjAyrCXtekOCHaHieoZQ110', 
       appName: 'Hello Cypress, This is Applitools!',
       testName: 'My first Cypress Test',
       browser: { width: 800, height: 600 },
